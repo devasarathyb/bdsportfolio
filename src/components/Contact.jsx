2da664009119fed73208a741164a5cab6fd74ea2
Devasarathy-B <devabalagurusamy@gmail.com>
2024-05-13 12:28:12 +0530

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
       <div className="flex flex-col items-center">
        
           <h1 className="text-4xl border-b-4  border-secondary  mb-5 w-[140px] font-bold">Contact</h1>
-          <p className="pb-5">To dicuss more in detail</p>
+          <p className="pb-5">To discuss more in detail</p>
           <p className="py-2"><span className="font-bold"> Email: </span>devabalagurusamy@gmail.com</p>
           <p className="py-2"><span className="font-bold"> Phone: </span>+91 6379182265</p>
          
